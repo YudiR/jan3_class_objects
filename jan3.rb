cfeae320ab3a5e7b4c958d14e3381c6bd5df897b
YudiR <yudrose13@gmail.com>
2019-01-05 18:48:00 -0500

    exercise 1 complete

---
+++ jan3.rb
@@ -0,0 +1,34 @@
+
+class Bank_Account
+     
+    def initialize(balance,intrest_rate)
+    @balance = balance
+    @intrest_rate = intrest_rate
+    end
+
+    def deposit(num)
+    @balance += num
+    return @balance
+    end 
+
+    def withdraw(num)
+        @balance -= num
+        return @balance
+    end
+
+    def total
+        intrest = @balance * @intrest_rate / 100
+         @balance  +=  intrest
+        return @balance
+    end
+end
+
+yudi = Bank_Account.new(55,10)
+
+puts yudi.deposit(55)
+
+
+
+puts yudi.withdraw(55)
+
+puts yudi.total
